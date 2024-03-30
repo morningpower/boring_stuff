581db8d899e9262634a6ffa17c65667410ae6d1d
Riki Asano <asanopower@gmail.com>
2024-03-30 12:31:58 +0900

    commit validateInput.py

---
+++ chap6/validateInput.py
@@ -0,0 +1,17 @@
+from asyncio.proactor_events import _ProactorDuplexPipeTransport
+from curses.ascii import isalnum
+
+
+while True:
+    print('年齢を入力してください:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('年齢は数字で入力してください。')
+
+while True:
+    print('新しいパスワードを入力してください（英数字のみ）:')
+    password = input()
+    if password.isalnum():
+        break
+    print('パスワードは英数字で入力してください。')
