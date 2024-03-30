2ef0fd08b0fb845459ef13cce7b271e0af89e303
Riki Asano <asanopower@gmail.com>
2024-03-30 12:54:27 +0900

    commit picnicTable.py

---
+++ chap6/picnicTable.py
@@ -0,0 +1,12 @@
+from turtle import right
+
+
+def print_picninc(items_dict, left_width, right_width):
+    print('PICNIC ITEMS'.center(left_width + right_width, '-'))
+    for k, v in items_dict.items():
+        print(k.ljust(left_width, '.') + str(v).rjust(right_width))
+
+
+picnic_items = {'sandwitch': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+print_picninc(picnic_items, 12, 5)
+print_picninc(picnic_items, 20, 6)
